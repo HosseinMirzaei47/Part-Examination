d87d91bf07c1fea7eb094f5907445693c45a6fae
hosseinmirzaei <hosseinhidd@gmail.com>
2020-10-01 16:32:28 +0330

    Final Commit.

--- app/src/main/java/com/example/myapplication/core/BindingAdapters.kt
+++ app/src/main/java/com/example/myapplication/core/BindingAdapters.kt
@@ -1,9 +1,12 @@
 package com.example.myapplication.core
 
+import android.graphics.Color
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.card.MaterialCardView
 
 @BindingAdapter("imageSource")
 fun setImageUrl(imageView: ImageView, imageSource: String?) {
@@ -15,4 +18,23 @@ fun setImageUrl(imageView: ImageView, imageSource: String?) {
             )
             .into(imageView)
     }
+}
+
+@BindingAdapter("backgroundColor")
+fun setColor(materialCardView: View, strength: Int) {
+
+    materialCardView as MaterialCardView
+
+    when {
+        strength < 50 -> {
+            materialCardView.setBackgroundColor(Color.YELLOW)
+        }
+        strength in 51..299 -> {
+            materialCardView.setBackgroundColor(Color.BLUE)
+        }
+        else -> {
+            materialCardView.setBackgroundColor(Color.RED)
+        }
+    }
+
 }
